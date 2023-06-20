70797eca959cba49a3cbf16ae0c024d8dca05268
DonOutcast <sham1996@yandex.ru>
2023-06-20 11:01:06 +0300

    check_list day 3 and 4

---
+++ check_list/04/Школа 21_files/static.js
@@ -0,0 +1,8 @@
+window.PRODUCT_VERSION = 'r__64.1.0-rc1_028c396d226__s21__STABLE' || undefined;
+window.KEYCLOAK_ENDPOINT = 'https://auth.sberclass.ru/auth' || undefined;
+window.SUPPORT_DOMAIN = 'support-s21.dmz.prod-ins2.pcbl.local' || undefined;
+window.KEYCLOAK_CLIENT = 'school21' || undefined;
+window.SENTRY_DSN = 'https://e4f3278a73844eb2bab49f2fd0d53c26@sentry.pcbltools.ru/4' || undefined;
+window.GTM_ID = 'GTM-KQ8HBZ2' || undefined;
+window.GITLAB_URL = 'https://repos.21-school.ru' || undefined;
+window.VIDEO_URL = 'https://cdn.sberclass.ru/services/storage/download' || undefined;
